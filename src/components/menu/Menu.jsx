f5577f59bdf03e6dd857b6caed5a1a8c985c52e8
aish <aishnikam31@gmail.com>
2024-01-09 01:15:07 +0530

    about section done

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -9,6 +9,9 @@ export default function Menu({ menuOpen, setMenuOpen }) {
           <a href="#intro">Home</a>
         </li>
         <li onClick={()=>setMenuOpen(false)}>
+          <a href="#about">About</a>
+        </li>
+        <li onClick={()=>setMenuOpen(false)}>
           <a href="#portfolio">Skills</a>
         </li>
         <li onClick={()=>setMenuOpen(false)}>
